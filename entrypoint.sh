71b7f4b05d3a23521c713baa38ff4635929460e5
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-03-23 14:54:18 +0600

    flashed everything

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-
-python manage.py makemigrations
-python manage.py migrate 
-python manage.py loaddata
-
-exec "$@"
--- todos/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .views import TodoViewSet
-
-router = routers.DefaultRouter()
-router.register('todos', TodoViewSet, basename='todos')
-
-urlpatterns = router.urls
